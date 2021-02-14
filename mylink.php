841cb0c3db443b376225756e306f952753ceedf6
oscarmiguel <carlo11171977@gmail.com>
2021-02-14 11:58:57 +0800

    adding mylink.php

---
+++ mylink.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>My link</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h3><?Php echo 'This is my link.'; ?></h3>
+</body>
+</html>
