63e7553066b288beb5302a9e35872523e021a2b9
oscarmiguel <carlo11171977@gmail.com>
2021-02-14 11:57:37 +0800

    adding first prelim exer

---
+++ prelim_exer1.php
@@ -0,0 +1,16 @@
+<?Php $var = 'PHP Tutorial'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title><?Php echo $var; ?></title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p>
+    <p><a href="mylink.php">Go to my link</a></p>
+</body>
+</html></html>
